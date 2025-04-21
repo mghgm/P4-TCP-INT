c0bd312a0b876ff83b8d9f4d9cd49eff64451050
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2025-04-21 12:45:59 +0330

    Add reserve bpf

---
+++ bpf/sockops-reserve/reserve.bpf.c
@@ -0,0 +1,55 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+
+#define BPF_SOCK_OPS_WRITE_HDR_OPT_CB 15
+#define BPF_SOCK_OPS_HDR_OPT_LEN_CB 14
+#define BPF_SOCK_OPS_WRITE_HDR_OPT_CB_FLAG 64
+
+#define DSCP (23 << 2)
+#define IPPROTO_IP 0
+#define IP_TOS 1
+
+
+SEC("sockops")
+int bpf_sockops_parse_tcp_options(struct bpf_sock_ops *skops) {
+    bpf_printk("Boom!");
+
+    if (skops->op == BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB) {
+        int rv;
+        rv = bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_WRITE_HDR_OPT_CB_FLAG);
+        if (rv < 0) {
+            bpf_printk("Failed to setup flag:: BPF_SOCK_OPS_PARSE_ALL_HDR_OPT_CB_FLAG, %d", rv);
+            return 1;
+        }
+        
+        int rc;
+        int dscp;
+        dscp = DSCP;
+        rc = bpf_setsockopt(skops, IPPROTO_IP, IP_TOS, &dscp, sizeof(dscp));
+        if (rv < 0) {
+            bpf_printk("Failed to set DSCP through:: bpf_setsockopt, %d", rc);
+            return 1;
+        }
+    } else if (skops->op == BPF_SOCK_OPS_HDR_OPT_LEN_CB) {
+        int rv;
+        rv = bpf_reserve_hdr_opt(skops, 0x0c, 0);
+        if (rv) {
+            bpf_printk("Failed to reserve option");
+        }
+    } else if (skops->op == BPF_SOCK_OPS_WRITE_HDR_OPT_CB) {
+        char opt[12] = {0x72, 0x0c, 0x00, 0x00,
+                        0x00, 0x00, 0x00, 0x00,
+                        0x00, 0x00, 0x00, 0x00};
+        int rv;
+        rv = bpf_store_hdr_opt(skops, (void *)opt, 0x0c, 0);
+        if (rv) {
+            bpf_printk("Failed to store opt\n");
+        }
+    }
+
+    return 1;
+}
+
+char _license[] SEC("license") = "GPL";
+
